baaccbc6ebcb64041e4de9128c7cda99a8dbd380
sandeepdas900 <33979182+sandeepdas900@users.noreply.github.com>
2024-12-30 11:34:02 +0530

    Set up CI with Azure Pipelines
    [skip ci]

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,9 +1,9 @@
 provider "aws" {
-  region = "ap-south-1"  # Replace with your AWS region
+  region = "ap-south-1"  
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Example AMI, choose the correct one based on your needs
+  ami           = "ami-0c55b159cbfafe1f0"  
   instance_type = "t2.micro"
 }
 
