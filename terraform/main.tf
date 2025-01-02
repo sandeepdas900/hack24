49b89bd9973e63e86fe0c653ed0acbe8317544ff
sandeepdas900 <33979182+sandeepdas900@users.noreply.github.com>
2025-01-02 23:08:36 +0530

    Updated or created Terraform configuration via API

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,24 +3,36 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "gdiujfgdkjf" {
+resource "aws_s3_bucket" "example" {
   bucket = "gdiujfgdkjf"
 }
 
-resource "aws_vpc" "SouthRegionVPC" {
+resource "aws_vpc" "main" {
   cidr_block           = "10.0.0.0/16"
   enable_dns_support   = true
   enable_dns_hostnames = true
+
+  tags = {
+    Name = "SouthRegionVPC"
+  }
 }
 
-resource "aws_subnet" "SouthRegionSubnet" {
-  vpc_id                  = aws_vpc.SouthRegionVPC.id
-  cidr_block              = "10.0.1.0/24"
+resource "aws_subnet" "main" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "SouthRegionSubnet"
+  }
 }
 
-resource "aws_instance" "demoforhack24" {
-  ami                         = "ami-0c55b159cbfafe1f0"
-  instance_type               = "t2.micro"
-  subnet_id                   = aws_subnet.SouthRegionSubnet.id
+resource "aws_instance" "example" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.main.id
+
+  tags = {
+    Name = "demoforhack24"
+  }
 }
 ```
