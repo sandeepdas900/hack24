ac6a97e55e1fe5e9ea3a80fc76607b4c54a5a778
sandeepdas900 <33979182+sandeepdas900@users.noreply.github.com>
2025-01-03 22:03:01 +0530

    Updated or created Terraform configuration via API

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,152 +3,37 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_s3_bucket" "website_bucket" {
-  bucket = "my-song-collection"
-  acl    = "public-read"
+resource "aws_s3_bucket" "gdiujfgdkjf" {
+  bucket = "gdiujfgdkjf"
+}
 
-  website {
-    index_document = "index.html"
-    error_document = "error.html"
-  }
+resource "aws_vpc" "SouthRegionVPC" {
+  cidr_block           = "10.0.0.0/16"
+  enable_dns_support   = true
+  enable_dns_hostnames = true
 
-  versioning {
-    enabled = true
-  }
-
-  cors_rule {
-    allowed_headers = ["*"]
-    allowed_methods = ["GET"]
-    allowed_origins = ["*"]
-    max_age_seconds = 3000
-  }
-
-  lifecycle_rule {
-    enabled = true
-
-    noncurrent_version_expiration {
-      days = 30
-    }
+  tags = {
+    Name = "SouthRegionVPC"
   }
 }
 
-resource "aws_s3_bucket_policy" "website_bucket_policy" {
-  bucket = aws_s3_bucket.website_bucket.id
+resource "aws_subnet" "SouthRegionSubnet" {
+  vpc_id                  = aws_vpc.SouthRegionVPC.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true
 
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "s3:GetObject"
-        Effect    = "Allow"
-        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
-        Principal = "*"
-      },
-    ]
-  })
-}
-
-resource "aws_cloudfront_distribution" "website_distribution" {
-  origin {
-    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
-    origin_id   = "S3-my-song-collection"
-  }
-
-  enabled             = true
-  is_ipv6_enabled     = true
-  default_root_object = "index.html"
-
-  default_cache_behavior {
-    allowed_methods  = ["GET", "HEAD"]
-    cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-my-song-collection"
-
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-  }
-
-  price_class = "PriceClass_100"
-
-  viewer_certificate {
-    acm_certificate_arn = aws_acm_certificate.website_cert.arn
-    ssl_support_method  = "sni-only"
+  tags = {
+    Name = "SouthRegionSubnet"
   }
 }
 
-resource "aws_acm_certificate" "website_cert" {
-  domain_name = "example.com"
-  validation_method = "DNS"
-}
+resource "aws_instance" "demoforhack24" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  subnet_id     = aws_subnet.SouthRegionSubnet.id
 
-resource "aws_route53_zone" "website_zone" {
-  name = "example.com"
-}
-
-resource "aws_route53_record" "website_alias" {
-  zone_id = aws_route53_zone.website_zone.zone_id
-  name    = "example.com"
-  type    = "A"
-
-  alias {
-    name                   = aws_cloudfront_distribution.website_distribution.domain_name
-    zone_id                = aws_cloudfront_distribution.website_distribution.hosted_zone_id
-    evaluate_target_health = false
+  tags = {
+    Name = "demoforhack24"
   }
-}
-
-resource "aws_iam_role" "lambda_execution_role" {
-  name = "lambda_execution_role"
-
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action    = "sts:AssumeRole"
-        Effect    = "Allow"
-        Principal = {
-          Service = "lambda.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
-
-resource "aws_iam_policy" "lambda_policy" {
-  name = "lambda_policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents",
-        ]
-        Effect   = "Allow"
-        Resource = "*"
-      },
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
-  role       = aws_iam_role.lambda_execution_role.name
-  policy_arn = aws_iam_policy.lambda_policy.arn
-}
-
-resource "aws_lambda_function" "example_lambda" {
-  function_name = "example_lambda"
-  runtime       = "nodejs14.x"
-  role          = aws_iam_role.lambda_execution_role.arn
-  handler       = "index.handler"
-  filename      = "example_lambda.zip"
 }
 ```
