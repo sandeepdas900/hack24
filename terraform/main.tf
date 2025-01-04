8ad678d7bffbb56fde1aabffeabba712f702c602
sandeepdas900 <33979182+sandeepdas900@users.noreply.github.com>
2025-01-04 11:39:31 +0530

    Updated or created Terraform configuration via API

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,42 +1,195 @@
 ```hcl
 provider "aws" {
-  region = "us-east-1"
+  region = "us-west-2"
 }
 
-resource "aws_vpc" "SouthRegionVPC" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_support   = true
-  enable_dns_hostnames = true
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = "my-song-collection"
+  acl    = "public-read"
 
-  tags = {
-    Name = "SouthRegionVPC"
+  website {
+    index_document = "index.html"
+    error_document = "error.html"
   }
 }
 
-resource "aws_subnet" "SouthRegionSubnet" {
-  vpc_id            = aws_vpc.SouthRegionVPC.id
-  cidr_block        = "10.0.1.0/24"
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
 
-  tags = {
-    Name = "SouthRegionSubnet"
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website_bucket.arn}/*"
+      }
+    ]
+  })
+}
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id   = "S3-my-song-collection"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+    }
+  }
+
+  enabled             = true
+  is_ipv6_enabled     = true
+  comment             = "CDN for my song collection"
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "S3-my-song-collection"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
   }
 }
 
-resource "aws_s3_bucket" "gdiujfgdkjfdf" {
-  bucket = "gdiujfgdkjfdf"
+resource "aws_lambda_function" "backend_processing" {
+  function_name = "song-processing"
+  runtime       = "nodejs14.x"
+  role          = aws_iam_role.lambda_exec.arn
+  handler       = "index.handler"
 
-  tags = {
-    Name = "gdiujfgdkjfdf"
+  code {
+    s3_bucket = "lambda-functions"
+    s3_key    = "song-processing.zip"
   }
 }
 
-resource "aws_instance" "demoforhack24" {
-  ami           = "ami-0c55b159cbfafe1f0"
+resource "aws_iam_role" "lambda_exec" {
+  name = "lambda_exec_role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action    = "sts:AssumeRole"
+        Effect    = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_policy_attachment" "lambda_policy_attachment" {
+  name       = "lambda_policy_attachment"
+  roles      = [aws_iam_role.lambda_exec.name]
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_route53_zone" "dns_zone" {
+  name = "example.com"
+}
+
+resource "aws_route53_record" "cloudfront_alias" {
+  zone_id = aws_route53_zone.dns_zone.zone_id
+  name    = "www.example.com"
+  type    = "A"
+
+  alias {
+    name                   = aws_cloudfront_distribution.cdn.domain_name
+    zone_id                = aws_cloudfront_distribution.cdn.hosted_zone_id
+    evaluate_target_health = false
+  }
+}
+
+resource "aws_dynamodb_table" "song_metadata" {
+  name         = "SongMetadata"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "SongID"
+
+  attribute {
+    name = "SongID"
+    type = "S"
+  }
+}
+
+resource "aws_security_group" "ec2_security_group" {
+  name        = "ec2_security_group"
+  description = "Allow traffic to EC2 instances"
+  vpc_id      = "vpc-xxxxxxxx"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "backend_instance" {
+  ami           = "ami-xxxxxxxx"
   instance_type = "t2.micro"
-  subnet_id     = aws_subnet.SouthRegionSubnet.id
+  security_groups = [aws_security_group.ec2_security_group.name]
+}
 
-  tags = {
-    Name = "demoforhack24"
+resource "aws_elb" "web_elb" {
+  name = "web-load-balancer"
+
+  availability_zones = ["us-west-2a", "us-west-2b"]
+
+  listener {
+    instance_port     = 80
+    instance_protocol = "HTTP"
+    lb_port           = 80
+    lb_protocol       = "HTTP"
   }
+
+  health_check {
+    target              = "HTTP:80/"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+  }
+
+  instances = [aws_instance.backend_instance.id]
 }
 ```
